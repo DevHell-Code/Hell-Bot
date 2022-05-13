8a7694d9a091d3d779b71cfb7283ba85d5ebad63
Hyunmin Koo <62460314+hminkoo10@users.noreply.github.com>
2022-05-13 10:01:12 +0000

    Setting

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand
+from discord_slash import SlashContext
+from discord_slash.utils import manage_commands
+import random
+import asyncio
+import os
+from keep_alive import keep_alive
+
+
+bot.run(os.getenv("token"))
