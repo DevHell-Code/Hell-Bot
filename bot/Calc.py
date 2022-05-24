4d7997aae6e635bf204a815a206d61cccb0ac66a
KeySpace <77488170+froggal@users.noreply.github.com>
2022-05-24 06:06:50 +0000

    계산기 기능 파일 추가

---
+++ bot/Calc.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+import random
+
+# 임배드 함수
+def embed(title,description,color=random.randint(0x000000,0xFFFFFF)):
+    return discord.Embed(title=title,description=description,color=color)
+  
