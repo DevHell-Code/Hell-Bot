e9ad49a3a7bd022237782ae80aad525b9fda8690
KeySpace <77488170+froggal@users.noreply.github.com>
2022-05-24 06:15:13 +0000

    덧셈 기능 구현

--- bot/Calc.py
+++ bot/Calc.py
@@ -5,4 +5,17 @@ import random
 # 임배드 함수
 def embed(title,description,color=random.randint(0x000000,0xFFFFFF)):
     return discord.Embed(title=title,description=description,color=color)
-  
+
+class Calc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    # 사칙연산
+    @commands.command()
+    async def 사칙연산(self, ctx, type, left: int, right: int):
+      if type == '덧셈' :
+        data = left + right
+        await ctx.reply(embed=embed('사칙연산 in 덧셈', f'{left}+{right} = ||{data}||'))
+
+
+def setup(bot):
+    bot.add_cog(Calc(bot))
