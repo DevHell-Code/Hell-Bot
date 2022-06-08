6eee7e0f78d923e6790b8a7777edb9feb9c80ff1
KeySpace <77488170+froggal@users.noreply.github.com>
2022-06-08 06:25:36 +0000

    메시지 삭제 기능 손봄 & work.md NOTICE.md로 변경 In Manage.py & work.md & NOTICE.md

--- bot/Manage.py
+++ bot/Manage.py
@@ -13,13 +13,16 @@ class Manage(commands.Cog):
     @commands.command()
     async def 삭제(ctx, *, amount=999999999999999999999):
         if ctx.author.guild_permissions.manage_messages:
-            await ctx.channel.purge(limit=amount)
+            try:
+                await ctx.channel.purge(limit=amount)
+            except:
+                await ctx.reply(embed=embed("삭제 실패","https://bit.ly/joinbota를 이용해 메시지 관리 권한과 함께 초대해주세요.",discord.Color.red()))
             if amount == 999999999999999999999:
                 await ctx.reply(embed=embed("삭제 완료","이 채널에 있는 모든 메시지(매우 오래된 메시지 제외)가 삭제되었습니다!",discord.Color.green()))
             else:
-                await ctx.reply(embed=embed("삭제 완료",f"{amount}개의 메시지가 삭제되었습니다!",colo))
+                await ctx.reply(embed=embed("삭제 완료",f"{amount}개의 메시지가 삭제되었습니다!",discord.Color.green()))
         else:
-            await ctx.reply('메시지 관리권한이 없습니다. https://bit.ly/joinbota를 이용해 관리 권한과 함께 초대해주세요.')
+            await ctx.reply(embed=embed("삭제 실패",'메시지 관리권한이 없습니다.',discord.Color.red()))
     
 def setup(bot):
     bot.add_cog(Manage(bot))
