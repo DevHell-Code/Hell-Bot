b9dd98fba692f792efe24a5000004642d8814344
KeySpace <77488170+froggal@users.noreply.github.com>
2022-06-09 06:16:00 +0000

    오타 수정; http://hellcode.cf -> https://bit.ly/joinbota 헬코드 씨에프는 접속이 불가능하고, 초대 링크가 아니므로 비틀리 초대링크로 대체합니다. in bot/Manage.py

--- bot/Manage.py
+++ bot/Manage.py
@@ -4,7 +4,6 @@ import random
 import datetime
 import requests
 
-
 # 임배드 함수
 def embed(title, description, color=random.randint(0x000000, 0xFFFFFF)):
     return discord.Embed(title=title, description=description, color=color)
@@ -35,7 +34,7 @@ class Manage(commands.Cog):
             try:
                 await ctx.channel.purge(limit=amount)
             except:
-                await ctx.reply(embed=embed("삭제 실패","http://hellcode.cf를 이용해 메시지 관리 권한과 함께 초대해주세요.",discord.Color.red()))
+                await ctx.reply(embed=embed("삭제 실패","https://bit.ly/joinbota 를 이용해 메시지 관리 권한과 함께 초대해주세요.",discord.Color.red()))
             if amount == 999999999999999999999:
                 await ctx.reply(embed=embed("삭제 완료","이 채널에 있는 모든 메시지(매우 오래된 메시지 제외)가 삭제되었습니다!",discord.Color.green()))
             else:
@@ -44,15 +43,15 @@ class Manage(commands.Cog):
             await ctx.reply(embed=embed("삭제 실패",'메시지 관리권한이 없습니다.',discord.Color.red()))
     @commands.command()
     async def 타임아웃(self, ctx, user:discord.Member, min:int):
-        if not ctx.author.guild_permissions.timeout_members:
-            return await ctx.reply(embed=embed("타임아웃 실패","필수 요구사항인 **멤버 타임아웃**권한이 없습니다",discord.Color.red()))
+        if not ctx.author.guild_permissions.moderate_members:
+            return await ctx.reply(embed=embed("타임아웃 실패","필수 요구사항인 **멤버 관리**권한이 없습니다",discord.Color.red()))
         handshake = await timeout_user(bot=self.bot, user_id=user.id, guild_id=ctx.guild.id, until=min)
         if handshake:
              return await ctx.send(embed=embed("타임아웃 완료",f"{user}님을 {min}분동안 타임아웃 시켰습니다.",discord.Color.green()))
     @commands.command()
     async def 타임아웃해제(self, ctx, user:discord.Member):
-        if not ctx.author.guild_permissions.timeout_members:
-            return await ctx.reply(embed=embed("타임아웃 해제 실패","필수 요구사항인 **멤버 타임아웃**권한이 없습니다",discord.Color.red()))
+        if not ctx.author.guild_permissions.manage_members:
+            return await ctx.reply(embed=embed("타임아웃 해제 실패","필수 요구사항인 **멤버 관리**권한이 없습니다",discord.Color.red()))
         handshake = await remove_timeout_user(bot=self.bot, user_id=user.id, guild_id=ctx.guild.id)
         if handshake:
              return await ctx.reply(embed=embed("타임아웃 해제 완료",f"{user}님의 타임아웃을 해제시켰습니다.",discord.Color.green()))
