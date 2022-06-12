59442fbe3938985019b930a22f0950b73415e3d9
Hyunmin Koo <62460314+hminkoo10@users.noreply.github.com>
2022-06-12 01:15:52 +0000

    .

--- bot/Game.py
+++ bot/Game.py
@@ -9,39 +9,6 @@ import json
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
-def make_image(message):
-
-    W = 640
-    H = 640
-    bg_color = 'rgb(214, 230, 245)'
-    
-    font = ImageFont.truetype('font.ttf', size=28)
-    font_color = 'rgb(0, 0, 0)' # or just 'black'
-		# 원래 윈도우에 설치된 폰트는 아래와 같이 사용 가능하나,
-		# 아무리 해도 한글 폰트가 사용이 안 되어.. 같은 폴더에 다운받아 놓고 사용함.
-		# font = ImageFont.truetype("arial.ttf", size=28)
-    
-    image =Image.new('RGB', (W, H), color = bg_color)
-    draw = ImageDraw.Draw(image)
-    
-    # Text wraper to handle long text
-	# 40자를 넘어갈 경우 여러 줄로 나눔
-    lines = textwrap.wrap(message, width=40)
-  
-    # start position for text
-    x_text = 50
-    y_text = 50
-    
-    # 각 줄의 내용을 적음
-    for line in lines:
-        width, height = font.getsize(line)
-        draw.text((x_text, y_text), line, font=font, fill=font_color)
-        y_text += height
-        # height는 글씨의 높이로, 한 줄 적고 나서 height만큼 아래에 다음 줄을 적음
-        
-    # 안에 적은 내용을 파일 이름으로 저장
-    image.save("text")
-
 file = open("bot/sentences_kor.txt", "r")
 line = file.readlines()
 sentenses_kor = []
@@ -175,33 +142,19 @@ class Game(commands.Cog):
         bg_color = 'rgb(214, 230, 245)'
         
         font = ImageFont.truetype('font.ttf', size=28)
-        font_color = 'rgb(0, 0, 0)' # or just 'black'
-            # 원래 윈도우에 설치된 폰트는 아래와 같이 사용 가능하나,
-            # 아무리 해도 한글 폰트가 사용이 안 되어.. 같은 폴더에 다운받아 놓고 사용함.
-            # font = ImageFont.truetype("arial.ttf", size=28)
-        
+        font_color = 'rgb(0, 0, 0)'
         image =Image.new('RGB', (W, H), color = bg_color)
         draw = ImageDraw.Draw(image)
-        
-        # Text wraper to handle long text
-        # 40자를 넘어갈 경우 여러 줄로 나눔
         if arg == "한글":
             lines = textwrap.wrap(q, width=20)
         elif arg == "영어":
             lines = textwrap.wrap(q, width=30)
-      
-        # start position for text
         x_text = 50
         y_text = 50
-        
-        # 각 줄의 내용을 적음
         for line in lines:
             width, height = font.getsize(line)
             draw.text((x_text, y_text), line, font=font, fill=font_color)
             y_text += height
-            # height는 글씨의 높이로, 한 줄 적고 나서 height만큼 아래에 다음 줄을 적음
-            
-        # 안에 적은 내용을 파일 이름으로 저장
         image.save("text.png")
         file = discord.File("text.png", filename="text.png")
         start_time = time.time()
